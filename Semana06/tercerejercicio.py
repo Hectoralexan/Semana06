27acf4fb2a611caa0f7c6c90755e3c9a1a00411c
Hectoralexa <87905201+Xanalpha@users.noreply.github.com>
2021-09-04 10:05:31 -0600

    Create tercerejercicio.py

---
+++ Semana06/tercerejercicio.py
@@ -0,0 +1,10 @@
+compra =float(input("valor de la compra:"))
+tipo = str(input("pagaris al contado o con targeta de credito:"))
+if tipo == "contado":
+     descuento = compra *0.05
+     compra = compra - descuento
+     print("se agrego descuento:",compra)
+elif tipo =="targeta de credito":
+     aumento = compra *0.03
+     compra = compra  + aumento
+     print("se aplico aumento:", compra)
